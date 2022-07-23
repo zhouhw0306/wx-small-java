51006ad4025c53bb38f12f5ddd1f97ec9674daec
zhouhw0306 <2466864587@qq.com>
2022-07-23 18:23:05 +0800

    完善页面
    完善页面

--- pages/detailSpringboot/detailSpringboot.js
+++ pages/detailSpringboot/detailSpringboot.js
@@ -1,5 +1,4 @@
-// pages/challenge/challenge.js
-const db = wx.cloud.database()
+// pages/detailSpringboot/detailSpringboot.js
 Page({
 
     /**
@@ -8,29 +7,6 @@ Page({
     data: {
 
     },
-
-    //提交表单,添加进数据库
-    btnSub(res){
-        var type = parseInt(res.detail.value.type); //转number类型
-        var title = res.detail.value.title;
-        var context = res.detail.value.context;
-        //var {type,title,context} = res.detail.value;
-        //var resVlu = res.detail.value;
-        wx.showLoading({
-          title: '数据提交中',
-        })
-        db.collection("topicList").add({
-            data:{
-                type : type,
-                title : title,
-                context : context
-            }
-        }).then(res=>{
-            console.log(res)
-            wx.hideLoading()
-        })
-    },
-
 
     /**
      * 生命周期函数--监听页面加载
